322a3272e4ab9427c1f56f5723a1abb9ea7bbd36
Ramkumar <ramkumar0495@gmail.com>
2023-03-09 13:36:46 +0530

    login

--- src/test/java/org/tcs/GitSample.java
+++ src/test/java/org/tcs/GitSample.java
@@ -2,4 +2,16 @@ package org.tcs;
 
 public class GitSample {
 
+	private void test1() {
+		System.out.println("test1");
+
+	}
+
+	private void test2() {
+		System.out.println("test2");
+	}
+
+	private void test3() {
+		System.out.println("test3");
+	}
 }
